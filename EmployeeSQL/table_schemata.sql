5457341cc8f67c11ea4a9cdfcbdf2d4d29286541
Jenni <jenniferjaurequi7@gmail.com>
2024-08-04 18:14:13 -0700

    modified erd, code for table schemata and queries

---
+++ EmployeeSQL/table_schemata.sql
@@ -0,0 +1,85 @@
+
+-- Data Engineering: Table Schemata
+
+--------------------------------------------------------------------------------------------
+
+-- create tables
+
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INTEGER NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR NOT NULL PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INTEGER NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL PRIMARY KEY,
+    salary INTEGER NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- import data
+
+COPY titles (title_id, title)
+FROM 'C:/repos/sql-project/data/titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY employees (emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date)
+FROM 'C:/repos/sql-project/data/employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY departments (dept_no, dept_name)
+FROM 'C:/repos/sql-project/data/departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_emp (emp_no, dept_no)
+FROM 'C:/repos/sql-project/data/dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_manager (dept_no, emp_no)
+FROM 'C:/repos/sql-project/data/dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY salaries (emp_no, salary)
+FROM 'C:/repos/sql-project/data/salaries.csv'
+DELIMITER ','
+CSV HEADER;
+
+--------------------------------------------------------------------------------------------
+-- Qs
+-- accurately define value length for columns? 
