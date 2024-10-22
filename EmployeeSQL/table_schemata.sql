4eb7e741780acd36f1332a14cbb118f3e523a550
Jenni <jenniferjaurequi7@gmail.com>
2024-10-21 18:18:48 -0700

    specified varchar lengths

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -6,36 +6,36 @@
 -- create tables
 
 CREATE TABLE titles (
-    title_id VARCHAR NOT NULL PRIMARY KEY,
-    title VARCHAR NOT NULL
+    title_id VARCHAR(5) NOT NULL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no INTEGER NOT NULL PRIMARY KEY,
-    emp_title_id VARCHAR NOT NULL,
+    emp_title_id VARCHAR (50) NOT NULL,
     birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    sex VARCHAR (1) NOT NULL,
     hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL PRIMARY KEY,
-    dept_name VARCHAR NOT NULL
+    dept_no VARCHAR (50) NOT NULL PRIMARY KEY,
+    dept_name VARCHAR (50) NOT NULL
 );
 
 CREATE TABLE dept_emp (
     emp_no INTEGER NOT NULL,
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR (6) NOT NULL,
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR (6) NOT NULL,
     emp_no INTEGER NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
@@ -80,4 +80,5 @@ FROM 'C:/repos/sql-project/data/salaries.csv'
 DELIMITER ','
 CSV HEADER;
 
+----- drop tables to recreate
 
